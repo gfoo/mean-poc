d44d0ad9b3af233a134193f36d5105e84dde9eef
gilles <gilles.faucherand@gmail.com>
2017-11-07 11:32:26 +0100

    fix js var visibility

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from '../../services/auth.service';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private router: Router,
     private flashMessagesService: FlashMessagesService
   ) { }
